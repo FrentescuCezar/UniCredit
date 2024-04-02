c4791ae9fc8ce32c3cef54bdc53d186e6f51a922
cezar <79227201+FrentescuCezar@users.noreply.github.com>
2024-04-02 14:59:58 +0300

    Small Refactory Controller RequiredArgsConstructor

--- pfm/pfm-category-service/src/main/java/com/pfm/category/controller/CategoryController.java
+++ pfm/pfm-category-service/src/main/java/com/pfm/category/controller/CategoryController.java
@@ -1,26 +1,21 @@
 package com.pfm.category.controller;
 
-import com.pfm.category.repository.model.KeywordEntity;
 import com.pfm.category.service.dto.TransactionDTO;
 import com.pfm.category.service.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/categories")
 public class CategoryController {
     private final CategoryService categoryService;
-    @Autowired
-    public CategoryController(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
 
     @PostMapping("/findCategoryForTransaction")
     public ResponseEntity<Long> findCategoryForTransaction(@RequestBody TransactionDTO transactionDTO) {
@@ -28,8 +23,4 @@ public class CategoryController {
         return categoryId.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
-
-    
-
-
 }
