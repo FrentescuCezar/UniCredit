079b2ba4cc0d9c17f72e034d7d8f0a2e634ed0b7
cezar <79227201+FrentescuCezar@users.noreply.github.com>
2024-04-09 02:15:32 +0300

    Updated TransactionDTO with @NotNull for PutMapping

---
+++ pfm/pfm-transaction-service/src/main/java/com/pfm/transaction/service/dto/OnUpdate.java
@@ -0,0 +1,4 @@
+package com.pfm.transaction.service.dto;
+
+public interface OnUpdate {
+}
--- pfm/pfm-transaction-service/src/main/java/com/pfm/transaction/service/dto/TransactionDTO.java
+++ pfm/pfm-transaction-service/src/main/java/com/pfm/transaction/service/dto/TransactionDTO.java
@@ -1,6 +1,7 @@
 package com.pfm.transaction.service.dto;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,18 +10,22 @@ import java.util.Date;
 
 @Data
 @Builder
-public class TransactionDTO{
-
+public class TransactionDTO {
+    @NotNull(groups = OnUpdate.class) // Required for PUT
     private Date date;
 
+    @NotNull(groups = OnUpdate.class)
     private BigDecimal amount;
 
+    @NotNull(groups = OnUpdate.class)
     private Long categoryId;
 
+    @NotNull(groups = OnUpdate.class)
     private Long keywordId;
 
+    @NotNull(groups = OnUpdate.class)
     private Long parentId;
 
+    @NotNull(groups = OnUpdate.class)
     private String description;
-
 }
