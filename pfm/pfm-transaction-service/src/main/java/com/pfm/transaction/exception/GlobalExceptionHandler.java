29cbd7015dcc501f39ec4be9e75f46a49b7c04df
cezar <79227201+FrentescuCezar@users.noreply.github.com>
2024-04-09 02:14:54 +0300

    Updated ApiErrorResponse + Added methodArgumentNotValid
    Example of MethodArgumentNotValid:
    
    {
      "status": 400,
      "message": "Validation error",
      "timestamp": "2024-04-09T02:12:01.7814149",
      "validationErrors": [
        "parentId: must not be null",
        "categoryId: must not be null",
        "keywordId: must not be null",
        "amount: must not be null",
        "date: must not be null"
      ]
    }

--- pfm/pfm-transaction-service/src/main/java/com/pfm/transaction/exception/GlobalExceptionHandler.java
+++ pfm/pfm-transaction-service/src/main/java/com/pfm/transaction/exception/GlobalExceptionHandler.java
@@ -2,8 +2,12 @@ package com.pfm.transaction.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,5 +15,15 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiErrorResponse> handleTransactionNotFoundException(TransactionNotFoundException ex) {
         ApiErrorResponse errorResponse = new ApiErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        ApiErrorResponse apiError = new ApiErrorResponse(HttpStatus.BAD_REQUEST, "Validation error", errors);
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
     }
 }
